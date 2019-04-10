caa08e4081795f27e092f618fd5c12792a9e5563
SaraRoberts <sara.roberts@selu.edu>
2019-04-09 22:09:14 -0500

    Created Model
    Also files from first run

---
+++ CatIndex/CatIndex/Models/CatStats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatIndex.Models
+{
+    public class CatStats
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public Char Gender { get; set; }
+        public String Picture { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime Until { get; set; }
+        public Double Age { get; set; }
+        public String[] Likes { get; set; }
+        public String[] Dislikes { get; set; }
+        public String Bio { get; set; }
+        public String Breed { get; set; }
+    }
+}
